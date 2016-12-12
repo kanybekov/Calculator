15ead14a0afd5d19345aed0c1e72b46bd05969d4
slooooowpanda <akanybekov@gmail.com>
2016-12-13 03:47:30 +0500

    Fixed test a bit

--- CalculatorTests/CalculationTests.cs
+++ CalculatorTests/CalculationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using CalculatorLibrary.Calculation;
 using CalculatorLibrary.ExpressionPack;
@@ -9,34 +10,29 @@ using Ninject;
 namespace CalculatorTests
 {
     [TestFixture]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public class CalculationTests
     {
-        private IExpressionBuilder buider;
+        private IExpressionBuilder expressionBuilder;
 
         [OneTimeSetUp]
         public void Setup()
         {
             var kernel = new StandardKernel();
             kernel.Load(Assembly.GetExecutingAssembly());
-            buider = kernel.Get<IExpressionBuilder>();
+            expressionBuilder = kernel.Get<IExpressionBuilder>();
         }
 
         [TestCase("( 2 + 3 * 4 ) / 7", 2)]
         [TestCase("1 + 2 + 3 - 4 * 2", -2)]
         public void CalculateExpressionTest(string input, int expected)
         {
-            buider.CreateExpression(input);
-            var expression = buider.CreateExpression(input);
+            expressionBuilder.CreateExpression(input);
+            var expression = expressionBuilder.CreateExpression(input);
 
             var actual = expression.Evaluate();
 
             Assert.AreEqual(expected,actual);
-        }
-
-        [TestCase("1 + 3 - 2", 3)]
-        public void Test2(string input)
-        {
-            Mock<IExpressionBuilder> mock = new Mock<IExpressionBuilder>();
         }
     }
 }
